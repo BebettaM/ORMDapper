7d82421544173a6ebb07c00f550c16d39bbf7616
Bebetta Motondo <claramotondo2@yahoo.fr>
2023-04-07 11:50:08 -0500

    ORM and Dapper

---
+++ DapperExercise/Department.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace DapperExercise
+{
+	public class Department
+	{
+		public Department()
+		{
+		}
+		public int DepartmentID { get; set; }
+		public string Name { get; set; }
+	}
+}
+
